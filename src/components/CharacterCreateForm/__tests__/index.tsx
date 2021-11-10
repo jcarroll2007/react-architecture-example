ba149a5069675dcc1276a25b0839e8833b35c469
Jordan Carroll <jcarroll2007@gmail.com>
2021-11-10 08:23:15 -0500

    Update index.tsx

--- src/components/CharacterCreateForm/__tests__/index.tsx
+++ src/components/CharacterCreateForm/__tests__/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { CharacterCreateForm } from '..'
 
 describe('CharacterCreateForm', () => {
-  it('should not submit for and show error message if name is null', async () => {
+  it('should not submit form and show error message if name is null', async () => {
     const onSubmitMock = jest.fn()
     const { getByTestId, queryByTestId } = render(
       <CharacterCreateForm onSubmit={onSubmitMock} />
@@ -15,14 +15,6 @@ describe('CharacterCreateForm', () => {
     userEvent.click(submitButton)
     expect(onSubmitMock).not.toHaveBeenCalled()
 
-    // const nameInput = getByLabelText('Name')
     expect(getByTestId('name-error-message')).toBeInTheDocument()
-
-    // TODO: figure out why this typing isn't working
-    // await userEvent.type(nameInput, 'a')
-    // expect(nameInput).toHaveAttribute('value', 'a')
-
-    // await userEvent.click(submitButton)
-    // expect(onSubmitMock).toHaveBeenCalled()
   })
 })
